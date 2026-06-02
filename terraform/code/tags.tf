a8f79b87fdc3975669b3f11ad64e2081e54d9ed1
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-06-02 15:48:33 +0000

    Updating Tag Reference to GitHub

--- terraform/code/tags.tf
+++ terraform/code/tags.tf
@@ -1,5 +1,5 @@
 module "tags" {
-  source        = "git::https://dev.azure.com/CPSDTS/Case%20Progression/_git/tf-module-common-tags?ref=main"
+  source        = "git::https://github.com/CPS-Innovation/CPS-TF-Module-Common-Tags.git?ref=main"
   pipeline_name = var.pipeline_name
   repo_name     = var.repo_name
   branch_name   = var.branch_name
